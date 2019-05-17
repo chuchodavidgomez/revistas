c78ee3df24fadaed91b0de89cb6f66db516b782a
chucho <jesus.gomezo@udea.edu.co>
2019-05-17 13:21:10 -0500

    cambiar

--- src/funciones/Funciones_varias.java
+++ src/funciones/Funciones_varias.java
@@ -3,6 +3,7 @@ package funciones;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
@@ -96,34 +97,36 @@ public class Funciones_varias {
         final WebClient webClient = new WebClient();
 
         try {
-            webClient.getOptions().setJavaScriptEnabled(false);
+            webClient.getOptions().setJavaScriptEnabled(true);
             webClient.getOptions().setCssEnabled(false);
             //Pagina donde se hara la consulta
-            HtmlPage page1 = webClient.getPage("https://www.google.com/webhp");
+            System.out.println("-----------"+webClient.getOptions().toString());
+            HtmlPage page1 = webClient.getPage("http://ulrichsweb.serialssolutions.com/search/1786591");
             
             //nombre del formulario
             final HtmlForm form = page1.getForms().get(0);
             //el valor "f" no es arbitrario es el nombre del formulario web de google
-            
+            //DomElement domElement = page1.getElementById("cb_list");
+            //domElement.click();
             //nombre de la caja de texto
-            final HtmlTextInput textField = form.getInputByName("q");
+            //final HtmlTextInput textField = form.getInputByName("q");
             //el valor "q" no es arbitrario es el nombre de la caja de texto del formulario web de google
 
             //nombre del boton del formulario
-            final HtmlSubmitInput button = form.getInputByName("btnK");
+            //final HtmlSubmitInput button = form.getInputByName("btnK");
             //el valor "btnG" no es arbitrario es el nombre del boton del formulario web de google
                         
             //llenamos la caja de texto
-            textField.setValueAttribute("usandojava");
+            //textField.setValueAttribute("usandojava");
 
             //Creamos la pagina que nos devolverá el resultado
             HtmlPage pageResultado;
             
             //hacemos clic en el boton del formulario y asignamos el resultado a la pagina pageResultado
-            pageResultado = button.click();
+            //pageResultado = button.click();
             
             //imprimimos el resultado
-            System.out.println(pageResultado.asText());
+            System.out.println(page1.asText());
             
             //cerramos el navegador emulado, para liberar todo esto de la memoria
             webClient.close();
